0e3d7d4e5568119c4ca23498f54a00665ffd2974
Naoki Hamada <hmkz@s101.xrea.com>
2017-12-09 22:25:02 +0900

    Implement a qsub wrapper

--- examples/job_scheduler/job.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./sphere_2D.sh $parameters
--- examples/job_scheduler/qsub_wrapper.sh
+++ examples/job_scheduler/qsub_wrapper.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-env parameters=$@ qsub -V job.sh -o out -N abc
-while [ `qstat | grep abc` ]
+
+set -e
+
+# Make a unique job name by MD5 for avoiding name confliction
+name=$(md5sum <(echo $*) | cut -d' ' -f1)
+
+# File names for the standard output and the standard error
+outfile=${name}.out
+errfile=${name}.err
+
+# Submit an evaluation job
+qsub -N ${name} -o ${outfile} -e ${errfile} sphere_2D.sh ${parameters}
+
+# Wait for the job completion
+while [ $(qstat | grep ${name}) ]
 do
     sleep 10
 done
-tail -n1 out
+
+# Write an evaluation value
+tail -n1 ${outfile}
+
+# Clear temporary files
+rm -f ${outfile} ${errfile}
